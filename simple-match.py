6f41f9f9c8fcfffc1f40fa2b4432a57259a0a75e
Tamour <justtaimoor@gmail.com>
2023-05-12 16:49:49 +0300

    new file:   simple-match.py

---
+++ simple-match.py
@@ -0,0 +1,13 @@
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+#
+# On branch main
+# Your branch is up to date with 'origin/main'.
+#
+# Changes to be committed:
+#	modified:   python-01.py
+#
+# Untracked files:
+#	simple math.txt
+#
